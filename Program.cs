75766de3000f92c5f6ebba1e5432692d41b2b520
Байсангур <maremaret85@gmail.com>
2026-02-24 01:01:34 +0500

    Update project to net8.0, fix Telegram.Bot version

--- Program.cs
+++ Program.cs
@@ -1,64 +1,58 @@
 ﻿using System;
+using System.IO;
+using System.Threading;
 using System.Threading.Tasks;
 using Telegram.Bot;
-using Telegram.Bot.Types.Enums;
+
 namespace MathPocket
 {
-
     class Program
-
     {
-
         private static TelegramBotClient _bot;
         private static BotHandler _handler;
 
         static async Task Main()
-
         {
-            
-            var token = Environment.GetEnvironmentVariable("BOT_TOKEN");
-            
             Console.OutputEncoding = System.Text.Encoding.UTF8;
-            token = System.IO.File.ReadAllText("token.txt");
-            _bot = new TelegramBotClient(token);
-            _handler = new BotHandler(_bot);
 
-            var me = await _bot.GetMe();
-
-            Console.WriteLine($"@{me.Username} работает... Нажми Enter для выхода");
-
-            using var cts = new CancellationTokenSource();
-            while (true)
+            try
             {
-                try
-                {
-                    _bot.StartReceiving(
-                updateHandler: _handler.HandleUpdateAsync,
-                errorHandler: _handler.HandleErrorAsync,
-                receiverOptions: new Telegram.Bot.Polling.ReceiverOptions
-                {
-                    AllowedUpdates = { }
-                }
-            );
-                    await Task.Delay(-1);
-                    Console.ReadLine();
-                    cts.Cancel();
-                }
-                catch (Exception ex)
-                {
-                    Console.WriteLine("Перезапуск через 5 секунд...");
-                    await Task.Delay(5000);
-                }
-            }
-            
+                // Чтение токена из файла
+                var token = File.ReadAllText("token.txt");
+                if (string.IsNullOrEmpty(token))
+                    throw new Exception("Файл token.txt пустой или токен не задан.");
 
-            
+                // Инициализация бота
+                _bot = new TelegramBotClient(token);
+                _handler = new BotHandler(_bot);
+
+                var me = await _bot.GetMe();
+                Console.WriteLine($"@{me.Username} работает... Нажми Enter для выхода");
+
+                using var cts = new CancellationTokenSource();
+
+                _bot.StartReceiving(
+                    updateHandler: _handler.HandleUpdateAsync,
+                    errorHandler: _handler.HandleErrorAsync,
+                    receiverOptions: new Telegram.Bot.Polling.ReceiverOptions
+                    {
+                        AllowedUpdates = { } // все типы обновлений
+                    }
+                );
+
+                Console.ReadLine();
+                cts.Cancel();
+            }
+            catch (FileNotFoundException ex)
+            {
+                Console.WriteLine("Ошибка: файл token.txt не найден!");
+                Console.WriteLine(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Произошла ошибка при запуске бота:");
+                Console.WriteLine(ex.Message);
+            }
         }
     }
-
-
 }
-
-
-
-
